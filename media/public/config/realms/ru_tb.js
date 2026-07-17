db071cacb8f912f6019a279b52b9ccf4e53dc522
wh1to <141561980+wh1to@users.noreply.github.com>
2026-07-17 15:52:31 +0700

    Create ru_tb.js

---
+++ media/public/config/realms/ru_tb.js
@@ -0,0 +1,49 @@
+GS.CommonMenu.configs.realm = {
+    "static_links": [
+        {
+            "url": "https://lesta.ru/shop/tb/",
+            "color": "ffbe4c",
+            "id": 143,
+            "title_text": "realm_link_title_prem_shop"
+        },
+        {
+            "url": "https://lesta.ru/support/",
+            "color": "ffffff",
+            "id": 134,
+            "title_text": "link_title_support"
+        },
+        {
+            "url": "https://card.lesta.ru/ordercard/tb/",
+            "color": "ffbe4c",
+            "id": 148,
+            "title_text": "realm_link_title_tanksblitz_card"
+        },
+        {
+            "url": "https://tanksblitz.ru/ru/news/common/sber-spasibo-bonus/?utm_campaign=tb-sberspasibo",
+            "color": "5aff8b",
+            "id": 149,
+            "title_text": "realm_link_title_tanksblitz_sberspasibo"
+        }
+    ],
+    "name": "runet",
+    "third_party_js": "(function(){var options = GS.CommonMenu.getOptions();(function(w,d,p){var f=d.getElementsByTagName('script')[0],j=d.createElement('script'),i;j.async=true;j.src='//'+p['script'];j.setAttribute('id', 'tenor-loader');for(i in p){if (p.hasOwnProperty(i)) {j.setAttribute('data-'+i,p[i]);}}f.parentNode.insertBefore(j,f);})(window,document,{'script':'tenor.lesta.ru/assets/clicks/static/tracker.js','realm':options['realm'],'project':options['project'],'service':options['service'],'cpm_url':'https://rdr.lesta.ru/'});})();",
+    "main_icon": "/media/public/img/jd8cbssiSMiOtPU4trQr1A.svg",
+    "leaderboard_mt_url": "https://tanki.su/ru/ratings/achievements/",
+    "user_links": [
+        {
+            "url": "https://lesta.ru/personal/",
+            "color": "ffffff",
+            "id": 40,
+            "title_text": "realm_link_title_personal"
+        },
+        {
+            "url": "https://lesta.ru/shop/redeem/?utm_content=bonus-code",
+            "color": "ffffff",
+            "id": 41,
+            "title_text": "link_title_bonus_code"
+        }
+    ],
+    "change_pass_url": "https://lesta.ru/personal/",
+    "bind_phone_url": "https://lesta.ru/personal/cellphone/bind/",
+    "main_url": "https://lesta.ru/?utm_content=lesta-icon"
+};
