e50e47ec858f5e498cb8f9e0eb2f41a36cd83c86
wh1to <141561980+wh1to@users.noreply.github.com>
2026-07-17 15:51:23 +0700

    Create ru.js

---
+++ media/public/config/games/ru.js
@@ -0,0 +1,132 @@
+GS.CommonMenu.configs.games = [
+    {
+        "color": "ff5000",
+        "tab_name": "project_tab_mt",
+        "tablet_keyart": "/media/public/img/mb1NNz-STOmh-ncTETtZHw.jpg",
+        "platforms": {
+            "windows": "Windows"
+        },
+        "logo": "/media/public/img/TxwG5kunTPiulxuOGcckZQ.svg",
+        "name": "mt",
+        "desktop_keyart": "/media/public/img/10xuhUGyRz63F9aDe3RZtw.jpg",
+        "url": "https://tanki.su/",
+        "icon_svg_body": "<svg width=\"25\" height=\"33\" viewBox=\"0 0 25 33\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n<path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M12.0419 17.9938L16.6671 20.9058L15.3696 15.5664L19.5685 12.0832L14.0813 11.7207L12.042 6.6015L10.0015 11.7207L4.51536 12.0833L8.7138 15.5664L7.41631 20.9058L12.0419 17.9938ZM0 22.395V5.25086L5.66196 0.5H18.4222L24.0842 5.25086V22.395L12.0421 32.5L0 22.395Z\" fill=\"currentColor\"/>\n</svg>",
+        "desc_title": "project_title_mt",
+        "desc_text": "project_desc_mt",
+        "extra_links": [
+            {
+                "url": "https://tanki.su/",
+                "notifying_started": 1746527446,
+                "id": 512,
+                "title_text": "link_title_game_website",
+                "notifying_hash": 6750280784
+            },
+            {
+                "url": "http://forum.tanki.su/",
+                "notifying_started": 1746527446,
+                "id": 513,
+                "title_text": "link_title_forum",
+                "notifying_hash": 8730166747
+            },
+            {
+                "url": "https://lesta.ru/support/products/mt/",
+                "notifying_started": 1746527446,
+                "id": 514,
+                "title_text": "link_title_support",
+                "notifying_hash": 924263822
+            },
+            {
+                "url": "https://lesta.ru/clans/wot",
+                "notifying_started": 1746527480,
+                "id": 515,
+                "title_text": "link_title_clans",
+                "notifying_hash": 429701996
+            }
+        ],
+        "icon": "/media/public/img/UYGNAwlURoelnPvrjhFb4Q.svg",
+        "mobile_keyart": "/media/public/img/SEhsFFKhSPGRPG0SXa0g-g.jpg"
+    },
+    {
+        "color": "1eb9ff",
+        "tab_name": "project_tab_mk",
+        "tablet_keyart": "/media/public/img/TdMnrFh-T9ilI-9qpxvARA.jpg",
+        "platforms": {
+            "windows": "Windows"
+        },
+        "logo": "/media/public/img/O_JlauP0RR2GDovL_U32Gw.svg",
+        "name": "mk",
+        "desktop_keyart": "/media/public/img/1gdJaclNQ_C4MWyzxID7vA.jpg",
+        "url": "https://korabli.su",
+        "icon_svg_body": "<svg width=\"30\" height=\"38\" viewBox=\"0 0 30 38\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n<path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M23.0357 0H6.96429L0 4.75V27.4444L15 38L30 27.4444V4.75L23.0357 0ZM6.42857 23.2222V16.8889L12.3214 21.1111H12.8571V16.3611H9.64286L8.57143 12.1389H12.8571V10.0278H11.25L10.1786 6.33333H19.8214L18.75 10.0278H17.1429V12.1389H21.4286L20.3571 16.3611H17.1429V21.1111H17.6786L23.5714 16.8889V23.2222L15 29.2917L6.42857 23.2222Z\" fill=\"black\"/>\n</svg>",
+        "desc_title": "project_title_mk",
+        "desc_text": "project_desc_mk",
+        "extra_links": [
+            {
+                "url": "https://korabli.su",
+                "notifying_started": 1746527777,
+                "id": 516,
+                "title_text": "link_title_game_website",
+                "notifying_hash": 3981456526
+            },
+            {
+                "url": "https://forum.korabli.su",
+                "notifying_started": 1746527777,
+                "id": 517,
+                "title_text": "link_title_forum",
+                "notifying_hash": 67174526
+            },
+            {
+                "url": "https://lesta.ru/support/products/mk/",
+                "notifying_started": 1746527777,
+                "id": 518,
+                "title_text": "link_title_support",
+                "notifying_hash": 4368832634
+            },
+            {
+                "url": "https://friends.korabli.su",
+                "notifying_started": 1746527777,
+                "id": 519,
+                "title_text": "mk-referral-link-text",
+                "notifying_hash": 7196109896
+            }
+        ],
+        "icon": "/media/public/img/x5BgrvDcRW2NmE8zZ0dvEg.svg",
+        "mobile_keyart": "/media/public/img/xmqn7wmWSeOEG5Da5Or1SA.jpg"
+    },
+    {
+        "color": "f49511",
+        "tab_name": "project_tab_tb",
+        "tablet_keyart": "/media/public/img/3hV3CdUOSzu7J3dxiAuIsg.jpg",
+        "platforms": {
+            "windows": "Windows",
+            "android": "Android",
+            "ios": "iOS",
+            "mac": "Mac"
+        },
+        "logo": "/media/public/img/32MmbSJGRaiUHe_z36t0Hw.svg",
+        "name": "tb",
+        "desktop_keyart": "/media/public/img/f_KsqM6KSrCBZQKRhGVnMg.jpg",
+        "url": "https://tanksblitz.ru/",
+        "icon_svg_body": "<svg id=\"Layer_1\" data-name=\"Layer 1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"27\" height=\"35\" viewBox=\"0 0 27 35\"><defs><style>.cls-1{fill:#fff;}</style></defs><path class=\"cls-1\" d=\"M15.64,12.05,27,0,21.58,3.12H7.14L2.46,6.77v15.6L5.93,26,0,32.86,23.79,22.29,14.7,31.71l-2.48-2.62-2.5.76,5,5.16L27,22.34V6.73L24,4.42,22.7,6.35l2,1.52v13.2ZM7.45,24.19,4.79,21.44V7.9L7.94,5.44H17.6l-10,5.84,4.07,8Z\" transform=\"translate(0 -0.01)\"/></svg>",
+        "desc_title": "project_title_tb",
+        "desc_text": "project_desc_tb",
+        "extra_links": [
+            {
+                "url": "https://tanksblitz.ru/",
+                "notifying_started": 1746527996,
+                "id": 520,
+                "title_text": "link_title_game_website",
+                "notifying_hash": 8447274220
+            },
+            {
+                "url": "https://lesta.ru/support/products/tb/",
+                "notifying_started": 1746527996,
+                "id": 521,
+                "title_text": "link_title_support",
+                "notifying_hash": 2711697190
+            }
+        ],
+        "icon": "/media/public/img/HxF1ITaCQfOemsgtzc4Kfw.svg",
+        "mobile_keyart": "/media/public/img/9bMWN7pHQHSrF70JHlqULQ.jpg"
+    }
+];
