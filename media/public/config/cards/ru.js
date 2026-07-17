3218acee120304e86570989c8cba317dbce21124
wh1to <141561980+wh1to@users.noreply.github.com>
2026-07-17 15:52:46 +0700

    Create ru.js

---
+++ media/public/config/cards/ru.js
@@ -0,0 +1,106 @@
+GS.CommonMenu.configs.cards = [
+    {
+        "icon_svg_body": "<svg width=\"25\" height=\"33\" viewBox=\"0 0 25 33\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n<path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M12.0419 17.9938L16.6671 20.9058L15.3696 15.5664L19.5685 12.0832L14.0813 11.7207L12.042 6.6015L10.0015 11.7207L4.51536 12.0833L8.7138 15.5664L7.41631 20.9058L12.0419 17.9938ZM0 22.395V5.25086L5.66196 0.5H18.4222L24.0842 5.25086V22.395L12.0421 32.5L0 22.395Z\" fill=\"currentColor\"/>\n</svg>",
+        "name": "mt",
+        "icon": "/media/public/img/UYGNAwlURoelnPvrjhFb4Q.svg",
+        "extra_links": [],
+        "color": "0e0e0e",
+        "goods_list": {
+            "gold": {
+                "dont_show_if_other_good_exists": false,
+                "get_text": "good_get_mt_gold",
+                "other_good": "",
+                "get_url": "https://lesta.ru/shop/mt/gold/",
+                "dont_show_if_no_value": false,
+                "have_url": "https://lesta.ru/shop/mt/gold/",
+                "title_text": "good_title_mt_gold",
+                "icon": "/media/public/img/QlYMaaRVRaO8Z74RpLfpPw.svg"
+            },
+            "premium_plus": {
+                "dont_show_if_other_good_exists": false,
+                "get_text": "good_get_mt_premium_plus",
+                "other_good": "",
+                "get_url": "https://lesta.ru/shop/mt/premium/",
+                "dont_show_if_no_value": false,
+                "have_url": "https://lesta.ru/shop/mt/premium/",
+                "title_text": "good_title_mt_premium_plus",
+                "icon": "/media/public/img/9BktCNtXSS-PzFVehTHbcQ.svg"
+            },
+            "premium": {
+                "dont_show_if_other_good_exists": true,
+                "get_text": "good_get_common_premium",
+                "other_good": "premium_plus",
+                "get_url": "https://lesta.ru/shop/base_premium/",
+                "dont_show_if_no_value": true,
+                "have_url": "https://lesta.ru/shop/base_premium/",
+                "title_text": "good_title_common_premium",
+                "icon": "/media/public/img/sH75EDDGS9W6ESFIYfhUuQ.svg"
+            },
+            "clan": {
+                "dont_show_if_other_good_exists": false,
+                "get_text": "good_title_find_clan",
+                "other_good": "",
+                "get_url": "https://lesta.ru/clans/wot/find_clan/",
+                "dont_show_if_no_value": false,
+                "have_url": "https://lesta.ru/clans/wot/{0}/",
+                "title_text": "good_title_mt_clan",
+                "icon": "/media/public/img/e4nHYei2QDGUTXA4bWhcCw.svg"
+            },
+            "subscription": {
+                "dont_show_if_other_good_exists": false,
+                "get_text": "good_buy_mt_subscription",
+                "other_good": "",
+                "get_url": "https://lesta.ru/shop/mt/main/subscription_mt",
+                "dont_show_if_no_value": false,
+                "have_url": "https://lesta.ru/shop/mt/main/subscription_mt",
+                "title_text": "good_title_mt_subscription",
+                "icon": "/media/public/img/fMvAL91MRxmiRlgoPF5cXw.svg"
+            }
+        },
+        "profile_url": "http://tanki.su/community/accounts/{0}-{1}/",
+        "hero_art": "/media/public/img/GkeE1Y1iTeWdzpk4W0Pj2w.png",
+        "game_name": "project_profile_mt"
+    },
+    {
+        "icon_svg_body": "<svg width=\"25\" height=\"32\" viewBox=\"0 0 25 32\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n<path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M19.1964 0H5.80357L0 4V23.1111L12.5 32L25 23.1111V4L19.1964 0ZM5.35714 19.5556V14.2222L10.2679 17.7778H10.7143V13.7778H8.03571L7.14286 10.2222H10.7143V8.44444H9.375L8.48214 5.33333H16.5179L15.625 8.44444H14.2857V10.2222H17.8571L16.9643 13.7778H14.2857V17.7778H14.7321L19.6429 14.2222V19.5556L12.5 24.6667L5.35714 19.5556Z\" fill=\"white\"></path>\r\n</svg>",
+        "name": "mk",
+        "icon": "/media/public/img/vKqkEivsQN2HATn09XZh2w.svg",
+        "extra_links": [],
+        "color": "0e0e0e",
+        "goods_list": {
+            "gold": {
+                "dont_show_if_other_good_exists": false,
+                "get_text": "good_get_mk_gold",
+                "other_good": "",
+                "get_url": "https://lesta.ru/shop/mk/gold/",
+                "dont_show_if_no_value": false,
+                "have_url": "https://lesta.ru/shop/mk/gold/",
+                "title_text": "good_title_mk_gold",
+                "icon": "/media/public/img/QKi9K26fSFi6_ArbGRn_ZA.svg"
+            },
+            "premium": {
+                "dont_show_if_other_good_exists": false,
+                "get_text": "good_get_mk_premium",
+                "other_good": "",
+                "get_url": "https://lesta.ru/shop/mk/premium/",
+                "dont_show_if_no_value": false,
+                "have_url": "https://lesta.ru/shop/mk/premium/",
+                "title_text": "good_title_mk_premium",
+                "icon": "/media/public/img/7wLoeg7oTlCNfeccIW2nng.svg"
+            },
+            "clan": {
+                "dont_show_if_other_good_exists": false,
+                "get_text": "link_title_clans",
+                "other_good": "",
+                "get_url": "https://clans.korabli.su/new-recommendations",
+                "dont_show_if_no_value": false,
+                "have_url": "https://clans.korabli.su/new-recommendations",
+                "title_text": "link_title_clans",
+                "icon": "/media/public/img/c9wi2I6ITMGCRgMvSAmW0A.svg"
+            }
+        },
+        "profile_url": "http://profile.korabli.su/statistics/{0}/",
+        "hero_art": "/media/public/img/LCrT8YzOTmKLF_n3shsGzQ.png",
+        "game_name": "project_profile_mk"
+    }
+];
